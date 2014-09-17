db15643c77ab5d33f998be3a1df9ce363d0311f6
XDdrummer <xddrummeronmc@gmail.com>
2014-09-17 12:32:23 -0400

    Update AdvWhitelist.java

--- src/com/xddrummer/advwhitelist/AdvWhitelist.java
+++ src/com/xddrummer/advwhitelist/AdvWhitelist.java
@@ -1,1 +1,20 @@
-null
+public class AdvWhitelist extends JavaPlugin {
+  
+  public static void onEnable(){
+    getLogger().info("Enabling Advanced Whitelist by XDdrummer.");
+    getLogger().info("If you downloaded this plugin from a source outside of PlanetMinecraft or Bukkit.org, please remove and download from XDdrummer's sources.");
+    
+    saveDefaultConfig();
+    
+    getServer().getPluginManager().registerEvents(new LoginListener, this);
+    
+  }
+  
+  public static void onDisable(){
+    getLogger().info("Disabling Advanced Whitelist by XDdrummer.");
+    getLogger().info("If you downloaded this plugin from a source outside of PlanetMinecraft or Bukkit.org, please remove and download from XDdrummer's sources.");
+  }
+  
+  
+  
+}

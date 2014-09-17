c5c2d9dd6d23e1bdbdd88385fd375f6c508241cf
XDdrummer <xddrummeronmc@gmail.com>
2014-09-17 12:38:23 -0400

    Update LoginListener.java

--- src/com/xddrummer/advwhitelist/listener/LoginListener.java
+++ src/com/xddrummer/advwhitelist/listener/LoginListener.java
@@ -1,1 +1,28 @@
-null
+public class LoginListener implements Listener {
+  
+  @EventHandler
+  public void preLogin(AsyncPlayerPreLoginEvent e){
+    
+  }
+  
+  @EventHandler
+  public void onJoin(PlayerJoinEvent e){
+    
+  }
+  
+  @EventHandler
+  public void onKick(PlayerKickEvent e){
+    
+  }
+  
+  @EventHandler
+  public void onLogin(PlayerLoginEvent e){
+    
+  }
+  
+  @EventHandler
+  public void onQuit(PlayerQuitEvent e){
+    
+  }
+
+}

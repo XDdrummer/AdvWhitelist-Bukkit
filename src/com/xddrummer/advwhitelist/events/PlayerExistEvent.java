fab47fb724e5bf52cd5b2cbaa26ec9430317f6a1
XDdrummer <xddrummeronmc@gmail.com>
2014-09-17 12:40:34 -0400

    Update PlayerExistEvent.java

--- src/com/xddrummer/advwhitelist/events/PlayerExistEvent.java
+++ src/com/xddrummer/advwhitelist/events/PlayerExistEvent.java
@@ -1,3 +1,22 @@
 public class PlayerExistEvent extends Event {
 
+    private static final HandlerList handlers = new HandlerList();
+    private String message;
+ 
+    public CustomEvent(String example) {
+        message = example;
+    }
+ 
+    public String getMessage() {
+        return message;
+    }
+ 
+    public HandlerList getHandlers() {
+        return handlers;
+    }
+ 
+    public static HandlerList getHandlerList() {
+        return handlers;
+    }
+
 }
